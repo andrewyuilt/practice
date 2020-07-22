98cfc03cf0a3869f00b4e618c4dedfdb4c5527e3
Andrew <dron8052@gmail.com>
2020-07-22 21:35:07 +0300

    second quest done

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 "use strict";
 const numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?","Введите количество");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat:false
+};
+
