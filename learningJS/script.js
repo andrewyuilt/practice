daf80691cffae1c703df6c8690acc03889c6cb45
Pandrew <dron8052@gmail.com>
2020-09-18 20:55:13 +0300

    lesson about __proto__

--- learningJS/script.js
+++ learningJS/script.js
@@ -1,95 +1,29 @@
 "use strict";
+// let str = "some";
+// let strObj = new String(str);
 
-// let a =5,
-//     b =a;
+// // console.log(typeof(str));
+// // console.log(typeof(strObj));
 
-// b = b + 5;
 
-// console.log(b);
-// console.log(a);
+// console.dir([1,2,3]);
 
-// const obj = {
-//     a: 5,
-//     b: 1
-// };
-
-// const copy = obj; //Ссылка
-
-// copy.a = 10;
-
-// console.log(copy);
-// console.log(obj);
-
-// function copy(mainObj) {
-//     let objCopy = {};
-
-//     let key;
-//     for (key in mainObj) {
-//         objCopy[key] = mainObj[key];
-//     }
-//     return objCopy;
-// }
-
-const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-        x: 7,
-        y: 4
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function() {
+        console.log("Hello");
     }
 };
 
-// const newNumbers = copy(numbers);
-// newNumbers.c.x = 10;
-// newNumbers.a = 10;
+const john = Object.create(soldier);
 
-// console.log(numbers);
-// console.log(newNumbers);
-
-// const add = {
-//     d: 17,
-//     e: 20
+// const john = {
+//     health: 100,
 // };
 
-// const clone = Object.assign({}, add);
+// john.__proto__ = soldier; //Устерел
 
-// clone.d = 20;
+// Object.setPrototypeOf(john, soldier);
 
-// // console.log(add);
-// // console.log(clone);
-
-// const oldArray = ['a', 'b', 'c'];
-// const newArray = oldArray.slice();
-
-// newArray[2] = 'Addidas';
-// console.log(oldArray);
-// console.log(newArray);
-
-const video = ['youtube', 'vimeo', 'rutube'],
-    blogs = ['wordpress', 'livejournal', 'blogger'],
-    internet = [...video, ...blogs, 'vk', 'facebook'];
-
-    console.log(internet);
-
-    function log(a, b, c) {
-        console.log(a);
-        console.log(b);
-        console.log(c);
-    }
-
-    const num = [2, 5, 7];
-
-    log(...blogs);
-
-    const array = ['a', 'b'];
-
-    const newArary = [...array];
-
-    const q = {
-        one: 1,
-        two: 2
-    };
-
-    const newObj = {...q};
-
-    console.log(newObj);
+john.sayHello();
